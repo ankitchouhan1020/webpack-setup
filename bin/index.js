4551961d14fd8a00ee4bb4066bf8e33010b80a01
Ankit Chouhan <ankitchouhan1020@gmail.com>
2021-01-02 18:31:19 +0000

    :fire: Remove Unused Code

--- bin/index.js
+++ bin/index.js
@@ -20,47 +20,6 @@ program
     .action(action.addPlugin);
 
 program
-    .command('open [projectName]')
-    .alias('o')
-    .description("Open one of your saved projects")
-    .action(action.openProject);
-
-program
-    .command('add [projectDirectory]')
-    .alias('save')
-    .option('-u, --url [link]', 'Add a link to a repository to projects')
-    .description("Save current directory as a project")
-    .action(action.addProject);
-
-program
-    .command('remove [projectName]')
-    .description("Remove the project")
-    .action(action.removeProject);
-
-program
-    .command('seteditor [commandToOpen]')
-    .description("Set text editor to use")
-    .option('-f|--for-project [projectName]', 'set different editor for specific project')
-    .action(action.setEditor);
-
-program
-    .command('rmeditor [projectName]')
-    .description("Remove text editor to use")
-    .option('-a|--all', 'remove editors from all projects')
-    .action(action.rmEditor);
-
-program
-    .command('edit')
-    .description("Edit settings.json")
-    .action(action.editConfigurations);
-
-program
-    .command('getpath [projectName]')
-    .alias('gp')
-    .description("Get project path")
-    .action(action.getProjectPath);
-
-program
     .arguments("<command>")
     .action((command) => {
         console.log(`Command ${command} not found\n`);
@@ -72,7 +31,7 @@ program.usage("<command>")
 
 
 if (process.argv.length <= 2) { // If no command mentioned then output help
-    action.initWebpack();
+    action.addPlugin();
 }
 
 // Parse arguments
